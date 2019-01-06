87c3bda520a7dc3a6154e05488025884e6bb2a4b
Thezigzagtree <33034701+Thezigzagtree@users.noreply.github.com>
2019-01-06 15:46:38 +0300

    Submission for Task 21

--- api/serializers.py
+++ api/serializers.py
@@ -2,6 +2,20 @@ from rest_framework import serializers
 from restaurants.models import Restaurant, Item
 from django.contrib.auth.models import User
 
+class RegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only = True)
+    class Meta:
+        model = User
+        fields = ['username', 'password']
+
+        def create(self, validated_data):
+            u = validated_data['username']
+            password = validated_data['password']
+            new_user = User(username = u)
+            new_user.set_password(password)
+            new_user.save()
+            return validated_data
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
